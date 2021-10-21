6efe9261b9c17991975d0fe621b51f9b98124437
nodierUcaldas <nodier.1701115195@ucaldas.edu.co>
2021-10-21 13:18:23 -0500

    update main
    funciona el click a hoteles

--- Prueba_Netactica/main.py
+++ Prueba_Netactica/main.py
@@ -1,46 +1,39 @@
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 import time
-import pandas as pd
+#import pandas as pd
+
+#opciones de navegacion
+options = webdriver.ChromeOptions()
+options.add_argument('--start-maximized')
+options.add_argument('--disable-extensions')
+
+driver_path = 'C:\\Users\\nodierjose\\Documents\\GitHub\\pruebaNetactica\\chromedriver.exe'
+
+driver = webdriver.Chrome(driver_path, chrome_options=options)
 
 
+#acomodar a la pantalla
+'''
+driver.set_window_position(2000,0)
+driver.maximize_window()
+time.sleep(1)
+'''
 
-class Source: #1)
+driver.get('https://www.viajesexito.com/')
 
-    def __init__(self, domain, articles, urls_list):
-        self.source_name=""
-        self.domain=domain
-        self.articles=articles
-        self.urls_list = urls_list
 
-    def build_urls(self, domain, endPoints):#2)
-       list_comprehesion = []
-       for i in endPoints:
-           list_comprehesion.append(domain+i)
+WebDriverWait(driver, 5)
+content = driver.find_element_by_css_selector('span.icon-hotel hoteles'.replace(' ','.'))
+content.click()
 
-       """print(list_comprehesion)"""
-       self.urls_list=list_comprehesion
 
-       return list_comprehesion
-
-class Article: #3)
-
-    def _init_(self):
-        url=""
-        source=""
-        parsed=False
-        html=None
-        title=""
-        table_data=None
-
-    def parse_html_title(self, xpath): #4)
-        title_xpath=""
-
+'''
 if __name__ == '__main__':
     S=Source("http/",["info1","info2","info3"],[])
     print(S.build_urls(S.domain,S.articles))
-
+'''
 
 
